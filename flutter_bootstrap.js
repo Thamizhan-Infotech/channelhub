bbbd14e194f63c4d8b8f696e665abd50cf7f9c80
Mr Eagle <mreagle@Mrs-MacBook-Pro.local>
2026-01-10 06:29:29 +0530

    build update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "556642492"
+    serviceWorkerVersion: "801922196"
   }
 });
