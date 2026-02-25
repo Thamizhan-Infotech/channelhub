d94ad1bf559009ae19592dc9a3516738c243d83e
rajkumar <mreagle@Mrs-MacBook-Pro.local>
2026-02-25 08:03:29 +0530

    Update web build: Wed Feb 25 08:03:29 IST 2026

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ac6efcf5d13160813c9f3f7bcb28958d",
+const RESOURCES = {"flutter_bootstrap.js": "70e560a3f4b76aac21652dc1585f55f7",
 "version.json": "e349d55f479ab39e7a66d724a8f92b18",
 "index.html": "164423f1f767fceb21edcc0aca7dfbb7",
 "/": "164423f1f767fceb21edcc0aca7dfbb7",
-"main.dart.js": "b7ede48651778d50e34ef99a25a038ab",
+"main.dart.js": "0d778613865dfeabb76f640549842fca",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "430c75a7955bf0dc0d18c857ed442a59",
 "icons/Icon-192.png": "da0ececf2d4e86ceae6cc200dc07d00b",
