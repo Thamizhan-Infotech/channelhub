5155edcb766d035f3d8050186441f94e9a1ffee0
rajkumar <mreagle@Mrs-MacBook-Pro.local>
2026-03-31 18:05:52 +0530

    Update web build: Tue Mar 31 18:05:52 IST 2026

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1731814740" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2250449464" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
